2b8f3e9bb2bb04bc71bc20eec61633b4cded76ef
ilunnie <luisgustavocaris2004@gmail.com>
2024-07-29 16:00:06 -0300

    feature: neuron network

--- NeuralNetwork/neuralNetwork.c
+++ NeuralNetwork/neuralNetwork.c
@@ -5,18 +5,25 @@
 #include "structures.h"
 #include "activationFunctions.h"
 
-double neuron_output(Neuron *n, double* values) {
+double neuron_output(Neuron *n, double* inputs) {
     double result = 0;
     for (int i = 0; i < n->inputs; i++)
-        result += values[i] * n->weights[i];
+        result += inputs[i] * n->weights[i];
     result += n->bias;
     
     return n->activate(result);
 }
 
-double* layer_output(Layer *l, double* values) {
+double* layer_output(Layer *l, double* inputs) {
     double *output = (double*)malloc(l->count * sizeof(double));
     for (int i = 0; i < l->count; i++)
-        output[i] = neuron_output(&l->neurons[i], values);
+        output[i] = neuron_output(&l->neurons[i], inputs);
+    return output;
+}
+
+double* network_output(Network *nn, double* inputs) {
+    double* output = inputs;
+    for (int i = 0; i < nn->count; i++)
+        output = layer_output(&nn->layers[i], output);
     return output;
 }
--- NeuralNetwork/neuralNetwork.h
+++ NeuralNetwork/neuralNetwork.h
@@ -4,7 +4,8 @@
 #ifndef NEURAL_NETWORK_H
 #define NEURAL_NETWORK_H
 
-double neuron_output(Neuron *n, double* values);
-double* layer_output(Layer *n, double* values);
+double neuron_output(Neuron *n, double* inputs);
+double* layer_output(Layer *l, double* inputs);
+double* network_output(Network *nn, double* inputs);
 
 #endif
